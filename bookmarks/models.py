26859cc574a2b33ba29528e4fbd8d575abc0cc23
ugochukwu <ugokingsley5@gmail.com>
2016-09-02 14:11:36 -0700

    first 46 pages

---
+++ bookmarks/models.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+from django.contrib.auth.models import User
+from django.db import models
+
+class Link(models.Model):
+    url = models.URLField(unique=True)
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=200)
+    user = models.ForeignKey(User)
+    link = models.ForeignKey(Link)
+
